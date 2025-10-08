53d09196f228b9fc436ad59f5d611b96fdb13b9b
Parham Roohi <144388104+ParhamRoohi@users.noreply.github.com>
2025-10-08 19:02:31 +0330

    feat: update aria label

--- app/UI/layout/NavBar.tsx
+++ app/UI/layout/NavBar.tsx
@@ -42,7 +42,6 @@ function NavBar() {
         className={`xl:hidden ${
           isOpen ? "h-dvh w-fit p-4" : "w-full p-4 pb-0"
         } fixed bg-white top-0 right-0 flex flex-col items-center z-[999]`}
-        aria-label="Navigation Menu"
       >
         <div className="flex flex-row justify-between border-2 border-b-2 border-[#646464] items-center w-full p-5">
           <Link href={"/"} onClick={() => setIsOpen(false)}>
@@ -54,7 +53,10 @@ function NavBar() {
               className="w-[213px] h-6"
             />
           </Link>
-          <button onClick={() => setIsOpen(!isOpen)}>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            aria-label="Navigation Menu"
+          >
             {!isOpen ? (
               <svg
                 width="24"
